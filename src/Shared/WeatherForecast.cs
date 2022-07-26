69513ddbc38de2062f65703aa1399282bd5ba5b2
Brady Stroud <bradystroud@ssw.com.au>
2022-07-26 22:50:10 +1000

    ✨ Connect fetchdata page to Api

--- src/Shared/WeatherForecast.cs
+++ src/Shared/WeatherForecast.cs
@@ -8,5 +8,4 @@ public class WeatherForecast
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
     public string? Summary { get; set; }
-
 }
--- src/SharedUI/Interfaces/IPlatformService.cs
+++ src/SharedUI/Interfaces/IPlatformService.cs
@@ -1,4 +1,5 @@
 ﻿namespace SharedUI.Interfaces;
+
 public interface IPlatformService
 {
     string GetPlatformName();
