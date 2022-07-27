aec14b2389c2ce91b17d7c313066fa04a075d3dc
Brady Stroud [SSW] <bradystroud@ssw.com.au>
2022-07-27 21:39:32 +1000

    🧹 More clean up

--- src/WebUI/Program.cs
+++ src/WebUI/Program.cs
@@ -9,7 +9,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
 var httpClient = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
 builder.Services.AddScoped(_ => httpClient);
 builder.Services.AddScoped<IPlatformService, PlatformService>();
