6331c8b9839fb52eff1898c492f55cab27c5a667
Brady Stroud [SSW] <bradystroud@ssw.com.au>
2022-07-27 21:39:08 +1000

    🧹 Clean up solution

--- src/WebUI/Program.cs
+++ src/WebUI/Program.cs
@@ -9,7 +9,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-var httpClient = new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)};
+
+var httpClient = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
 builder.Services.AddScoped(_ => httpClient);
 builder.Services.AddScoped<IPlatformService, PlatformService>();
 builder.Services.AddScoped<IWeatherForecastClient>(_ => new WeatherForecastClient("https://localhost:7136", httpClient));
